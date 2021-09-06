2c56adb4e7c8bc0a7c7ca19ed8f689f939dc561c
mckiernantim <38140787+mckiernantim@users.noreply.github.com>
2021-09-06 12:52:05 -0400

    Update UserProvider.js

--- src/Providers/UserProvider.js
+++ src/Providers/UserProvider.js
@@ -1,29 +1,11 @@
 import React, { useState, useEffect, createContext } from "react";
-import { auth } from "../Services/Firebase";
-export const UserContext = createContext({user:null})
+
 
 export default (props) => {
     const [user, setUser] = useState(null);
-    useEffect(() => {
-        auth.onAuthStateChanged(async (user) => {
-            console.log("changing state of user")
-            console.log(user)
-            if(user){
-            const { displayName, email, photoURL } = user;
-            setUser({
-                displayName,
-                email,
-                photoURL
-            })
-            }else{
-                setUser(null)
-        }
-    })
-    }, []) 
+    useEffect(() => {}, []) 
     return (
-        <UserContext.Provider value={user}>
-            <div>{props.children}</div>
-        </UserContext.Provider>
+      
     ) 
 }
 
