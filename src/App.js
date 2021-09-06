ac97e8f3003a23ef691eec351585302b0361492e
mckiernantim <38140787+mckiernantim@users.noreply.github.com>
2021-09-06 12:59:50 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import logo from "./logo.svg";
+
 import "./App.css";
 import { LoginPage } from "./Pages/LoginPage";
 import { LoggedInPage }from "./Pages/LoggedInPage"
-import "firebase/auth";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import UserProvider from "./Providers/UserProvider";
 
@@ -10,7 +9,7 @@ function App() {
 
   return (
     <div className="App">
-      <UserProvider>
+     
         <Router>
           <Switch>
             <Route exact path="/">
@@ -25,7 +24,7 @@ function App() {
             <LoginPage />
           </Switch>
           </Router>
-      </UserProvider>
+ 
     </div>
   );
 }
