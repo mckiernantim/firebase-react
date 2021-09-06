615f321d5c7f299070ca16c03ed163fdfe71309b
mckiernantim <38140787+mckiernantim@users.noreply.github.com>
2021-09-06 12:39:13 -0400

    Update Login.js

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,33 +1,20 @@
 import React, { useContext, useEffect} from "react";
-import { UserContext } from "../Providers/UserProvider";
-import { useHistory } from "react-router-dom";
-import { signInWithGoogle } from "../Services/Firebase";
+
 
 export const Login = () => {
 
   const history = useHistory()
-  const user = useContext(UserContext);
  
-  useEffect(() => {
-    if(user){
-      history.push("/LoggedInPage")
-    }
-  }, [user])
+  useEffect(() => {})
 
-  const handleSignIn = async () =>{
-    try {
-      await signInWithGoogle() 
-    } catch(err) {
-      console.log(err)
-    }
-  }
+  const handleSignIn = async () =>{}
  return (
     <div>
       <h1>Sign in with google!</h1>
       <button
         src="https://img.icons8.com/ios-filled/50/000000/google-logo.png"
         alt="google icon"
-        onClick={handleSignIn}
+        
       >
         <span> Continue with Google</span>
     </button>
