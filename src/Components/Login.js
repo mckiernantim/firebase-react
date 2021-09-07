67894f2d061a9a376472738bf6179cde39d25bae
mckiernantim <38140787+mckiernantim@users.noreply.github.com>
2021-09-07 16:19:15 -0400

    Update Login.js

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -2,19 +2,15 @@ import React, { useContext, useEffect} from "react";
 
 
 export const Login = () => {
-
   const history = useHistory()
- 
   useEffect(() => {})
-
-  const handleSignIn = async () =>{}
+const handleSignIn = async () =>{}
  return (
     <div>
       <h1>Sign in with google!</h1>
       <button
         src="https://img.icons8.com/ios-filled/50/000000/google-logo.png"
         alt="google icon"
-        
       >
         <span> Continue with Google</span>
     </button>
