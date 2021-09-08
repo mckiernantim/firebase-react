2610f22fc5ddf78a447417b891bec1b433a45d73
mckiernantim <38140787+mckiernantim@users.noreply.github.com>
2021-09-07 20:20:04 -0400

    Update UserProvider.js

--- src/Providers/UserProvider.js
+++ src/Providers/UserProvider.js
@@ -1,6 +1,12 @@
 import React, { useState, useEffect, createContext } from "react";
 
-
+const dummyUser = {
+  displayName: "Nic Cage",
+  email: " Nic@Cage.NicCage",
+  uid: "incomprehensible string",
+  photoURL:
+    "https://pyxis.nymag.com/v1/imgs/bef/c87/6359a2a35db664d45ac31794d122d4c8a7-15-nic-cage.rsocial.w1200.jpg",
+};
 export const UserProvider =  (props) => {
     const [user, setUser] = useState(null);
     useEffect(() => {}, []) 
