cdde34393698e302d0e985b0df3f3cb3610ab041
mckiernantim <38140787+mckiernantim@users.noreply.github.com>
2021-09-07 21:26:00 -0400

    Update LoggedInPage.js

--- src/Pages/LoggedInPage.js
+++ src/Pages/LoggedInPage.js
@@ -1,13 +1,10 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect } from "react";
 import { UserContext } from "../Providers/UserProvider";
 import { useHistory } from "react-router-dom";
-import { logOut } from "../Services/Firebase";
+
 
 export const LoggedInPage = () => {
   let history = useHistory();
-  const [displayName, setDisplayName] = useState("");
-  const [email, setEmail] = useState("");
-  const [photoURL, setPhotoURL] = useState("");
   
   const updateUser = () => {
 
@@ -22,13 +19,13 @@ export const LoggedInPage = () => {
   return (
     <div>
       <h1> YOU ARE NOW LOGGED IN</h1>
-      <h1>Welcome {displayName}!</h1>
-      <h1> WE KNOW YOUR EMAIL : {email} !</h1>
+      <h1>Welcome !</h1>
+      <h1> WE KNOW YOUR EMAIL : } !</h1>
       <div>
         <img
           className="user-image"
           alt="its the users head"
-          src={photoURL}
+          src=
           ></img>
           <h1>WE KNOW WHAT YOU LOOK LIKE</h1>
       </div>
