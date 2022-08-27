f3f81e3438569afe1b048eedbd6a909f8b0d2a26
mckiernantim <38140787+mckiernantim@users.noreply.github.com>
2022-08-27 11:41:53 -0400

    Update Firebase.js

--- src/Services/Firebase.js
+++ src/Services/Firebase.js
@@ -1,41 +1,42 @@
-
-
-import firebase from "firebase/app";
-import "firebase/auth";
+import { initializeApp } from 'firebase/app';
+import { GoogleAuthProvider, signInWithPopup, getAuth, signOut } from "firebase/auth";
 
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID
-};
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID
+  };
 
-const app = firebase.initializeApp(firebaseConfig);
-export const auth = app.auth();
+const app = initializeApp(firebaseConfig);
 
-const googleProvider = new firebase.auth.GoogleAuthProvider();
+const auth = getAuth();
+
+auth.useDeviceLanguage();
 
 
-export const signInWithGoogle = async () => {
-  try {
-  await auth.signInWithPopup(googleProvider);
-   } catch (err) {
-    console.log(err);
+const googleAuth = new GoogleAuthProvider();
+
+export const signInWithGoogle = () => {
+    try {
+  //the signInWithPopUp() method accepts ANY provider we create. This is all our authentication logic
+    signInWithPopup(auth, googleAuth).then((res) => {
+        console.log(res)
+    });
+     } catch (err) {
+      console.log(err);
+    }
+  };
+
+  export const logOut = async () =>{
+    try {
+      await signOut(auth)
+      alert("you've signed out - congrats.")
+    } catch(err) {
+      console.log(err)
+    }
   }
-};
-
-export const signOut = async () =>{
-  try {
-    await auth.signOut()
-    alert("you've signed out - congrats.")
-  } catch(err) {
-    console.log(err)
-  }
-}
-
-
-
